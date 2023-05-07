41bb1465044c1d0f93864d2291b4836af20b5f27
Brian Russell <brian@htpc.local>
2023-05-07 17:52:46 +0100

    Fixes for docker arguments

---
+++ start.sh
@@ -0,0 +1,4 @@
+systemctl --user enable container-binalerter.service
+systemctl --user enable container-binalerter.timer
+systemctl --user start  container-binalerter.timer
+systemctl --user start  container-binalerter.timer
