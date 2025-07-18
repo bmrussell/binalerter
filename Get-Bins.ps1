785c5dbeafc8e07c16d97ac01ed710684857f731
Brian Russell <noreply@github.com>
2025-07-18 11:23:22 +0100

    Shortened names

--- Get-Bins.ps1
+++ Get-Bins.ps1
@@ -9,6 +9,22 @@ class CollectionEvent {
     [Nullable[datetime]]$CollectionDate
 }
 
+function Map-Name {
+    param (
+        [string]$FullName
+    )
+
+    if ($FullName -eq "Mixed dry recycling (blue lidded bin) and glass (black box or basket)") {
+        return "Plastic & Glass"
+    } elseif ($FullName -eq "Chargeable garden waste") {
+        return "Garden"
+    } elseif ($FullName -eq "Household waste") {
+        return "Household"
+    } 
+    return $FullName
+}
+
+# Get the page that asks for postcode
 $uri = 'https://ilforms.wiltshire.gov.uk/WasteCollectionDays/index'
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add('Host', 'ilforms.wiltshire.gov.uk')
@@ -25,30 +41,26 @@ $headers.Add('Accept-Language', 'en-GB,en;q=0.9,en-US;q=0.8')
 $result = Invoke-WebRequest -Uri $uri -Headers $headers
 
 
-
+# Supply postcode
 $postParams = @{Postcode = $PostCode; Month = (Get-Date).Month; Year = (Get-Date).Year }
 $result = Invoke-WebRequest -Uri $ApiUrl -Method POST -Body $postParams
 $resultJson = ConvertFrom-Json -InputObject $result
 
+# Get the collections using the address ID from the first postcode address
 $addressId = $resultJson.Model.PostcodeAddresses[0].UPRN
 $postParams.Add('Uprn', $addressId)
-
 $postParams = @{Postcode = $PostCode; Month = (Get-Date).Month; Year = (Get-Date).Year; Uprn = $addressId }
 $result = Invoke-WebRequest -Uri $CollectionUrl -Method POST -Body $postParams
-
-
 $html = $result.Content
-# Patterns to extract each part of the date
-$regexDayNames = '<span\s+class="card-collection-day">\s*([A-Za-z]+)\s*</span>'
+
+# Get the day, month/year and collection type from the HTML
 $regexDayNumbers = '<span class="card-collection-date">\s*(\d{1,2})\s*</span>'
 $regexMonthYears = '<span class="card-collection-month">\s*(.*?)\s*</span>'
 $regexCollection = '<li class="collection-type-[^"]+">\s*(.*?)\s*</li>'
 
-# Extract each component of the date
-$daynames = [regex]::Matches($html, $regexDayNames) | ForEach-Object { $_.Groups[1].Value.Trim() }
 $daynumbers = [regex]::Matches($html, $regexDayNumbers) | ForEach-Object { $_.Groups[1].Value.Trim() }
 $monthyears = [regex]::Matches($html, $regexMonthYears) | ForEach-Object { $_.Groups[1].Value.Trim() }
-$collections = [regex]::Matches($html, $regexCollection) | ForEach-Object { if ($_.Groups[1].Value.Trim().substring(0,5) -eq "Mixed") { "Plastic & Glass" } else {$_.Groups[1].Value.Trim()}}
+$collections = [regex]::Matches($html, $regexCollection) | ForEach-Object { Map-Name $_.Groups[1].Value.Trim() }
 
 $collectionEvents = for ($i = 0; $i -lt $collections.Count; $i++) {
     [CollectionEvent]@{
@@ -56,12 +68,8 @@ $collectionEvents = for ($i = 0; $i -lt $collections.Count; $i++) {
         CollectionDate = [datetime]::ParseExact("$($daynumbers[$i]) $($monthyears[$i])", "d MMMM yyyy", $null)
     }
 }
-#$collectionEvents
 
-#$collectionEvents  | Where-Object { $_.CollectionDate -gt (Get-Date).Date } 
-
-
-
+# Format output, adding a '*' if the next collection is tomorrow
 $collectionEvents |
 Where-Object { $_.CollectionDate -gt (Get-Date).Date } |
 Select-Object CollectionType, @{Name = 'CollectionDate' 
@@ -77,4 +85,5 @@ Select-Object CollectionType, @{Name = 'CollectionDate'
     }    
         
 }
+
 
